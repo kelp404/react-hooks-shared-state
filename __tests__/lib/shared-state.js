efd8ac371edb01c30e6a9686c7b1d96c79cf9f08
Kelp <kelp@phate.org>
2019-05-03 07:06:25 +0800

    Added tests.

--- __tests__/lib/shared-state.js
+++ __tests__/lib/shared-state.js
@@ -1,3 +1,5 @@
+const React = require('react');
+const renderer = require('react-test-renderer');
 const SharedState = require('../../lib/shared-state');
 
 test('setState() is assign state and call setters.', () => {
@@ -17,9 +19,88 @@ test('setState() is assign state and call setters.', () => {
   expect(shareState.setters[1].set).toBeCalledWith({test: true});
 });
 
-test('Setter it was generated execute without path.', () => {
+test('Setter call setState() without path.', () => {
   const shareState = new SharedState();
   const setState = shareState.generateSetState();
+  jest.spyOn(shareState, 'setState');
   setState({test: true});
   expect(shareState.state).toMatchSnapshot();
+  expect(shareState.setState).toBeCalledWith({test: true});
+});
+
+test('Setter assign a partial state and call setters with path.', () => {
+  const shareState = new SharedState();
+  const setState = shareState.generateSetState('test');
+  shareState.setters = [
+    {
+      path: 'test',
+      set: jest.fn()
+    },
+    {
+      set: jest.fn()
+    }
+  ];
+  setState(true);
+  expect(shareState.state).toMatchSnapshot();
+  expect(shareState.setters[0].set).toBeCalledWith(true);
+  expect(shareState.setters[1].set).toBeCalledWith({test: true});
+});
+
+test('Call useState() to get the state and the setter.', () => {
+  const shareState = new SharedState();
+  jest.spyOn(shareState, 'useState');
+  shareState.setState({test: true});
+  const component = () => {
+    const [state, setState] = shareState.useState();
+    expect(state).toMatchSnapshot();
+    if (state.test) {
+      setState({test: false});
+    }
+
+    return React.createElement('div', null);
+  };
+
+  renderer.create(React.createElement(component, null));
+  expect(shareState.useState).toBeCalledTimes(2);
+});
+
+test('Call useState() with a initial state to get the state.', () => {
+  const shareState = new SharedState();
+  jest.spyOn(shareState, 'useState');
+  const component = () => {
+    const [state] = shareState.useState(null, {test: true});
+    expect(state).toMatchSnapshot();
+    return React.createElement('div', null);
+  };
+
+  renderer.create(React.createElement(component, null));
+  expect(shareState.useState).toBeCalled();
+});
+
+test('Call useState() with a path and a initial partial state to get the state.', () => {
+  const shareState = new SharedState();
+  jest.spyOn(shareState, 'useState');
+  const component = () => {
+    const [state] = shareState.useState('test', true);
+    expect(state).toMatchSnapshot();
+    return React.createElement('div', null);
+  };
+
+  renderer.create(React.createElement(component, null));
+  expect(shareState.useState).toBeCalled();
+});
+
+test('Call useState() twice to make sure it did not assign setter again.', () => {
+  const shareState = new SharedState();
+  jest.spyOn(shareState, 'useState');
+  shareState.setState({test: true});
+  const component = () => {
+    const [state] = shareState.useState();
+    expect(state).toMatchSnapshot();
+    return React.createElement('div', null);
+  };
+
+  renderer.create(React.createElement(component, null));
+  renderer.create(React.createElement(component, null));
+  expect(shareState.useState).toBeCalled();
 });
